b969055fe87b3079ed6437eedb0cd9f57ce433ba
ahmed al-hanini <a.hanini111@gmail.com>
2023-10-04 14:55:18 +0300

    j

--- lib/features/pokemon_image/presentation/providers/pokemon_image_provider.dart
+++ lib/features/pokemon_image/presentation/providers/pokemon_image_provider.dart
@@ -3,6 +3,8 @@ import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:dio/dio.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mapp_clean_architecture/core/constants/constants.dart';
+import 'package:flutter_mapp_clean_architecture/features/pokemon/business/entities/pokemon_entity.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../../core/connection/network_info.dart';
@@ -23,7 +25,8 @@ class PokemonImageProvider extends ChangeNotifier {
     this.failure,
   });
 
-  void eitherFailureOrPokemonImage() async {
+  void eitherFailureOrPokemonImage(
+      {required PokemonEntity pokemonEntity}) async {
     PokemonImageRepositoryImpl repository = PokemonImageRepositoryImpl(
       remoteDataSource: PokemonImageRemoteDataSourceImpl(
         dio: Dio(),
@@ -36,9 +39,16 @@ class PokemonImageProvider extends ChangeNotifier {
       ),
     );
 
+    String imageUrl = isShiny
+        ? pokemonEntity.sprites.other.officialArtwork.frontShiny
+        : pokemonEntity.sprites.other.officialArtwork.frontDefault;
+
     final failureOrPokemonImage =
         await GetPokemonImage(pokemonImageRepository: repository).call(
-      pokemonImageParams: PokemonImageParams(),
+      pokemonImageParams: PokemonImageParams(
+        name: pokemonEntity.name,
+        imageUrl: imageUrl,
+      ),
     );
 
     failureOrPokemonImage.fold(
